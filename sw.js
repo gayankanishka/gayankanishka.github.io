30e194c0d716d56b3f5957fcb5ccd1d93c397ae5
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-08-01 19:05:43 +0000

    Deploying to master from  @ e7bd3db4dca003c9723ed3fe4b7804f5c65713ae 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b7556e8bc457cbe279b9.js"
+    "url": "webpack-runtime-644d83afcd5556b755f0.js"
   },
   {
     "url": "framework-03ac347d0e7ee2a9d992.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f258aafcf9574addde69dadddc0ed591"
+    "revision": "56524ce3e65e25e8aab8857108e8f437"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-07e14503b2fc37a01f00.js"
