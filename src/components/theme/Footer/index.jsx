8a9d8c8c290bc6822bb10ea92b69d731e32874c1
gayankanishka <32380979+gayankanishka@users.noreply.github.com>
2020-08-02 13:13:41 +0530

    feat: get footer icon form cms

--- src/components/theme/Footer/index.jsx
+++ src/components/theme/Footer/index.jsx
@@ -1,22 +1,48 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby'
 import { Container } from 'components/common';
 import { Wrapper, Flex, Links, Details } from './styles';
-import social from './social.json';
 
-export const Footer = () => (
-  <Wrapper>
-    <Flex as={Container}>
-      <Details>
-        <h2>Gayan K</h2>
-        <span>© All rights are reserved | {new Date().getFullYear()} </span>
-      </Details>
-      <Links>
-        {social.map(({ id, name, link, icon }) => (
-          <a key={id} href={link} target="_blank" rel="noopener noreferrer" aria-label={`follow me on ${name}`}>
-            <img width="24" src={icon} alt={name} />
-          </a>
-        ))}
-      </Links>
-    </Flex>
-  </Wrapper>
-);
+export const Footer = () => {
+  const {
+        allContentfulSocialLinks: { edges } 
+    } = useStaticQuery(
+        graphql`
+        query FooterSocialLinks {
+            allContentfulSocialLinks(filter: {contentful_id: {eq: "3zKKRtQoxsMSMJQst1kaRz"}}) {
+                edges {
+                    node {
+                        id
+                        name
+                        link
+                        icon {
+                            file {
+                                url
+                            }
+                        }
+                    }
+                }
+            }
+        }
+      `
+    );
+  
+
+  return (
+    <Wrapper>
+      <Flex as={Container}>
+        <Details>
+          <h2>Gayan K</h2>
+          <span>© All rights are reserved | {new Date().getFullYear()} </span>
+        </Details>
+        <Links>
+          {edges.map(({ node }) => (
+            <a key={node.id} href={node.link} target="_blank" rel="noopener noreferrer" aria-label={`follow me on ${node.name}`}>
+              <img width="24" src={node.icon.file.url} alt={node.name} />
+            </a>
+          ))}
+        </Links>
+      </Flex>
+    </Wrapper>
+  );
+};
